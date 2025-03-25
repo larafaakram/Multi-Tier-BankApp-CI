da0eb8ee32b35ce88745be89bd04280131d6deac
Mohamed Akram LARAFA <larafa.akram@gmail.com>
2025-03-25 17:05:58 +0100

    update classe BankappApplicationTests, Add annotation ContextConfiguration

--- src/test/java/com/example/bankapp/BankappApplicationTests.java
+++ src/test/java/com/example/bankapp/BankappApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.bankapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
+@ContextConfiguration(classes = ElastSearchBootApplication.class)
 class BankappApplicationTests {
 
 	@Test
