faaa60085c2e037877586d9ee9e4cedb06fc461e
Mohamed Akram LARAFA <larafa.akram@gmail.com>
2025-03-25 17:32:31 +0100

    update classe BankappApplicationTests, Add annotation ContextConfiguration 3

--- src/test/java/com/example/bankapp/BankappApplicationTests.java
+++ src/test/java/com/example/bankapp/BankappApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.bankapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import com.example.ElastSearchBootApplication;
+//import com.example.ElastSearchBootApplication;
 
 
 @SpringBootTest
