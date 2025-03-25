65c7d03fc84cdad32aab54588d2ca343697a50cf
Mohamed Akram LARAFA <larafa.akram@gmail.com>
2025-03-25 18:05:03 +0100

    update classe BankappApplicationTests, Add annotation ContextConfiguration 6

---
+++ src/test/java/com/example/ElastSearchBootApplication.java
@@ -0,0 +1,12 @@
+package com.example; // Vérifiez que ce package est correct
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElastSearchBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElastSearchBootApplication.class, args);
+    }
+}
+
